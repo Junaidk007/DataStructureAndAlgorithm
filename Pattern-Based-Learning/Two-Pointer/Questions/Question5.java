b655e689ced7765adcc5d25fbb08d26e852fa3a9
Junaidk007 <junaid.k0004@gmail.com>
2026-06-11 21:50:56 +0530

    solved question no.5 and 6

--- Pattern-Based-Learning/Two-Pointer/Questions/Question5.java
+++ Pattern-Based-Learning/Two-Pointer/Questions/Question5.java
@@ -1,3 +1,46 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the 
+// squares of each number sorted in non-decreasing order.
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+
+
 public class Question5 {
-    
+    public static int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+
+        int[] ans = new int[nums.length];
+
+
+        while (i <= j) {
+            if (Math.abs(nums[i]) >= Math.abs(nums[j])) {
+                ans[j-i] = nums[i] * nums[i];
+                i++;
+            } else {
+                ans[j-i] = nums[j] * nums[j];
+                j--;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+
+        int[] ans = sortedSquares(nums);
+        
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
 }
--- Pattern-Based-Learning/Two-Pointer/Questions/Questions.md
+++ Pattern-Based-Learning/Two-Pointer/Questions/Questions.md
@@ -4,3 +4,5 @@
 2. [Separate/Segregate 0s and 1s](https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1)
 3. [Remove duplicates from sorted linked list](https://leetcode.com/problems/remove-duplicates-from-sorted-list/)
 4. [Remove duplicates from sorted array I](https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/)
+5. [Square and sort array](https://leetcode.com/problems/squares-of-a-sorted-array/description/)
+6. [Merge sorted arrays](https://leetcode.com/problems/merge-sorted-array/description/)
